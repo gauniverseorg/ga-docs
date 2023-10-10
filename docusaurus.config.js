2094e54e562db639e5af4ae81d4534523ae262c4
AbdulrahmanAlosaimi <abdulrahmanosaimi@gmail.com>
2023-10-10 13:30:52 +0300

    Modified config files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,6 +4,9 @@
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Docs",
@@ -79,6 +82,28 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+      typesense: {
+        // Replace this with the name of your index/collection.
+        // It should match the "index_name" entry in the scraper's "config.json" file.
+        typesenseCollectionName: "docs",
+
+        typesenseServerConfig: {
+          nodes: [
+            {
+              host: process.env.TYPESENSE_HOST,
+              port: process.env.TYPESENSE_PORT,
+              protocol: process.env.TYPESENSE_PROTOCOL,
+            },
+          ],
+          apiKey: process.env.TYPESENSE_API_KEY,
+        },
+
+        // Optional: Typesense search parameters: https://typesense.org/docs/0.24.0/api/search.html#search-parameters
+        typesenseSearchParameters: {},
+
+        // Optional
+        contextualSearch: true,
       },
     }),
 };
